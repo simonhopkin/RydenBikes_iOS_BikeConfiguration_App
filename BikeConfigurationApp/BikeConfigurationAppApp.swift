c69e55f568f5322d1480aa3e17a5ffa0413a20f8
Simon Hopkin <shopkin@nemaura.co.uk>
2024-09-05 17:27:41 +0100

    Initial Commit

---
+++ BikeConfigurationApp/BikeConfigurationAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BikeConfigurationAppApp.swift
+//  BikeConfigurationApp
+//
+//  Created by Simon Hopkin on 05/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct BikeConfigurationAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
