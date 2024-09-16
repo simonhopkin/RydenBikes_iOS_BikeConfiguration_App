21babacb8705a5225403683d5eafa1e7670b2872
Simon Hopkin <shopkin@nemaura.co.uk>
2024-09-16 15:54:39 +0100

    My Fit (WIP)

--- BikeConfigurationApp/Views/MeasureSaddlePositionView.swift
+++ BikeConfigurationApp/Views/MeasureSaddlePositionView.swift
@@ -173,7 +173,6 @@ struct MeasureSaddlePositionView: View {
         @Binding var isPresented: Bool
         @Binding var value: Double
         @State var captureAngle: Bool = false
-//        let motionManager = CMMotionManager()
         let motionManager = MotionManager(motionManager: CMMotionManager())
         
         var body: some View {
@@ -210,29 +209,6 @@ struct MeasureSaddlePositionView: View {
                         if newValue {
                             //dismiss keyboard if shown
                             UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
-//                            motionManager.startDeviceMotionUpdates(to: OperationQueue.main) { motion, error in
-//                                if let motion = motion {
-////                                    let roll = motion.attitude.roll * 180 / .pi
-////                                        let pitch = motion.attitude.pitch * 180 / .pi
-////                                        let yaw = motion.attitude.yaw * 180 / .pi
-//                                    
-//                                        let rotationMatrix = motion.attitude.rotationMatrix
-//                                                    
-//                                    let quaternion = motion.attitude.quaternion
-//                                    
-////                                        let roll = atan2(rotationMatrix.m32, rotationMatrix.m33)
-//                                    
-////                                    let pitch = asin(-rotationMatrix.m31) * 180 / .pi
-//                                    
-//                                    
-//                                    let pitch = atan2(2 * (quaternion.w * quaternion.y - quaternion.z * quaternion.x), 1 - 2 * (quaternion.x * quaternion.x + quaternion.y * quaternion.y)) * 180 / .pi
-//                                    
-//                                    value = pitch
-////                                        /*callback*/(roll, pitch, yaw)
-//                                }
-//                            }
-                            
-                            
                             do {
                                 try motionManager.startDeviceOrientationUpdates { roll, pitch, yaw in
                                     value = pitch
@@ -246,7 +222,6 @@ struct MeasureSaddlePositionView: View {
                             }
                         }
                         else {
-//                            motionManager.stopDeviceMotionUpdates()
                             motionManager.stopDeviceOrientationUpdates()
                         }
                     }
